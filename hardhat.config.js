209068e74e3e5c756b336287f739814dc50b8464
ocmv <orianacmv@gmail.com>
2022-08-06 13:12:48 -0400

    build(deploy): add deploy and config files

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 module.exports = {
   solidity: '0.8.9',
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${projectId}`,
+    goerli: {
+      url: `https://goerli.infura.io/v3/${projectId}`,
       accounts: [
         privateKey,
       ],
